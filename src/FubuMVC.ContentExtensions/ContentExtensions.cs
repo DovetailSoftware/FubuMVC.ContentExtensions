edf2ee7119d993590eb1430a6fbb12718d8b65f8
jmarnold <rncodepoet@gmail.com>
2012-09-18 13:58:37 -0500

    Initial code tree

---
+++ src/FubuMVC.ContentExtensions/ContentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using FubuMVC.Core;
+using FubuMVC.Core.View;
+
+namespace FubuMVC.ContentExtensions
+{
+    public static class ContentExtensions
+    {
+        public static IHtmlString WriteExtensions<T>(this IFubuPage<T> page) where T : class
+        {
+            return page.Get<ContentExtensionGraph>().ApplyExtensions(page);
+        }
+
+        public static IHtmlString WriteExtensions<T>(this IFubuPage<T> page, string tag) where T : class
+        {
+            return page.Get<ContentExtensionGraph>().ApplyExtensions(page, tag);
+        }
+
+        public static ExtensionsExpression Extensions(this FubuRegistry registry)
+        {
+            return new ExtensionsExpression(registry);
+        }
+    }
+}
