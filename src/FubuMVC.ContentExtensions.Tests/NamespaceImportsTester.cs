d996f6fe0d0f7e6ff7ab5485c1363de501088091
Corey Kaylor <corey@kaylors.net>
2013-02-16 13:32:02 -0700

    Register namespace with view engines

---
+++ src/FubuMVC.ContentExtensions.Tests/NamespaceImportsTester.cs
@@ -0,0 +1,24 @@
+﻿using FubuCore;
+using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.View;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuMVC.ContentExtensions.Tests
+{
+    [TestFixture]
+    public class NamespaceImportsTester
+    {
+        [Test]
+        public void ensure_namespaces_are_imported()
+        {
+            var registry = new FubuRegistry();
+            registry.Import<ContentExtensionsRegistration>();
+
+            var graph = BehaviorGraph.BuildFrom(registry);
+            var commonViewNamespaces = graph.Services.DefaultServiceFor<CommonViewNamespaces>().Value.As<CommonViewNamespaces>();
+            commonViewNamespaces.Namespaces.ShouldContain(typeof(ContentExtensionGraph).Namespace);
+        }
+    }
+}
---
+++ src/FubuMVC.ContentExtensions/ContentExtensionsRegistration.cs
@@ -0,0 +1,13 @@
+﻿using FubuMVC.Core;
+using FubuMVC.Core.View;
+
+namespace FubuMVC.ContentExtensions
+{
+    public class ContentExtensionsRegistration : IFubuRegistryExtension 
+    {
+        public void Configure(FubuRegistry registry)
+        {
+            registry.AlterSettings<CommonViewNamespaces>(x => x.AddForType<ContentExtensionsRegistration>());
+        }
+    }
+}
